692f520240116e1bfad8f1454c729051297d26af
sherif <sherifgamal9441@gmail.com>
2026-05-04 10:09:23 +0300

    fixed small bug in dvc.yaml, and pathing errors in main.py and save_preprocessing.py, with small chang in paths.yaml

--- src/saving_loading/save_preprocessing.py
+++ src/saving_loading/save_preprocessing.py
@@ -6,18 +6,18 @@ def save_data(X, y, x_path, y_path):
     # Ensure the directories exist dynamically
     Path(x_path).parent.mkdir(parents=True, exist_ok=True)
     
-    print("💾 Saving processed datasets...")
+    print("Saving processed datasets...")
     X.to_csv(x_path, index=False)
     y.to_csv(y_path, index=False)
-    print(f"✅ Data saved to: {Path(x_path).parent}")
+    print(f"Data saved to: {Path(x_path).parent}")
 
     return None
 
 def save_preprocessor(preprocessor, preprocessor_path):
     Path(preprocessor_path).parent.mkdir(parents=True, exist_ok=True)
     
-    print("📦 Saving fitted preprocessor artifact...")
-    joblib.save(preprocessor, preprocessor_path)
-    print(f"✅ Preprocessor saved to: {preprocessor_path}")
+    print("Saving fitted preprocessor artifact...")
+    joblib.dump(preprocessor, preprocessor_path)
+    print(f"Preprocessor saved to: {preprocessor_path}")
 
     return None
